6d5c6963c78b8c9eaed2d315d398238fc20fb754
Hasyimi Bahrudin <hasyimibahrudin@gmail.com>
2019-05-13 00:24:59 +0800

    Fix failing tests

--- message/handshake_test.go
+++ message/handshake_test.go
@@ -7,19 +7,18 @@ import (
 	"github.com/hasyimibhar/p2p-chat/ed25519"
 )
 
-func TestHandshake_EncodeDecode(t *testing.T) {
-	_, pub, _ := ed25519.GenerateKey()
-	msg := Handshake{
-		PublicKey: pub,
-		Addr:      "localhost:1234",
-	}
+func TestHandshake_EncodeDecodeVerify(t *testing.T) {
+	priv, pub, _ := ed25519.GenerateKey()
+	msg, _ := NewHandshake(priv, pub, "localhost:1234")
 
 	encoded, err := msg.Encode()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !bytes.Equal(encoded, append(pub, []byte("localhost:1234")...)) {
+	sig, _ := ed25519.Sign(priv, pub, append(pub, []byte("localhost:1234")...))
+
+	if !bytes.Equal(encoded, append(pub, append([]byte("localhost:1234"), sig...)...)) {
 		t.Fatal("encoded message is incorrect")
 	}
 
@@ -33,10 +32,17 @@ func TestHandshake_EncodeDecode(t *testing.T) {
 		t.Fatal("wrong message type")
 	}
 
+	if err := handshake.Verify(); err != nil {
+		t.Fatal("message signature is invalid")
+	}
+
 	if !bytes.Equal(handshake.PublicKey, pub) {
 		t.Fatal("decoded message is incorrect")
 	}
 	if handshake.Addr != "localhost:1234" {
+		t.Fatal("decoded message is incorrect")
+	}
+	if !bytes.Equal(handshake.Signature, sig) {
 		t.Fatal("decoded message is incorrect")
 	}
 }
