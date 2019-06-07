dc1df7d959a776841eef8675b06a4e30925a4665
Hasyimi Bahrudin <hasyimibahrudin@gmail.com>
2019-06-07 23:07:20 +0800

    Implement DistantNodes and FindNode

--- kademlia/routing.go
+++ kademlia/routing.go
@@ -2,9 +2,15 @@ package kademlia
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 type NodeID uint64
+
+type RoutingEntry struct {
+	NodeID   NodeID
+	NodeAddr string
+}
 
 type RoutingTable struct {
 	root *bucket
@@ -13,26 +19,83 @@ type RoutingTable struct {
 func NewRoutingTable(k int, owner NodeID, max NodeID) *RoutingTable {
 	return &RoutingTable{
 		root: &bucket{
-			K:     k,
-			Low:   0,
-			High:  max,
-			Owner: owner,
-			Nodes: []NodeID{},
+			K:       k,
+			Low:     0,
+			High:    max,
+			Owner:   owner,
+			Entries: []RoutingEntry{},
 		},
 	}
 }
 
 // Insert returns true if the node is inserted and false otherwise.
-func (t *RoutingTable) Insert(node NodeID) bool {
-	if !t.root.WithinRange(node) {
+func (t *RoutingTable) Insert(entry RoutingEntry) bool {
+	if !t.root.WithinRange(entry.NodeID) {
 		panic(fmt.Errorf("node id is outside of the entire id space"))
 	}
 
-	return t.root.Insert(node)
+	return t.root.Insert(entry)
 }
 
-func (t *RoutingTable) Nodes() []NodeID {
+func (t *RoutingTable) Nodes() []RoutingEntry {
 	return t.root.AllNodes()
+}
+
+// DistantNodes returns at most n nodes from a distant subtree randomly.
+func (t *RoutingTable) DistantNodes(n int) []RoutingEntry {
+	var nodes []RoutingEntry
+
+	if t.root.Left != nil && t.root.Right != nil {
+		if t.root.Left.Contains(t.root.Owner) {
+			nodes = t.root.Right.AllNodes()
+		} else {
+			nodes = t.root.Left.AllNodes()
+		}
+	} else {
+		nodes = t.root.Entries
+	}
+
+	if len(nodes) < n {
+		return nodes
+	}
+
+	rand.Shuffle(len(nodes), func(i, j int) {
+		nodes[i], nodes[j] = nodes[j], nodes[i]
+	})
+
+	return nodes[:n]
+}
+
+// FindNode returns at most k nodes that is closest to the target node.
+func (t *RoutingTable) FindNode(target NodeID) (candidates []RoutingEntry) {
+	candidates = []RoutingEntry{}
+
+	current := t.root
+	buckets := []*bucket{t.root}
+
+	// Build a list of k-buckets increasingly closer to the target node
+	for current.WithinRange(target) && current.Left != nil || current.Right != nil {
+		if current.Left.WithinRange(target) {
+			current = current.Left
+		} else {
+			current = current.Right
+		}
+
+		buckets = append(buckets, current)
+	}
+
+	// Select at most k nodes, starting from the closest k-bucket
+	for i := len(buckets) - 1; i >= 0; i-- {
+		for _, e := range buckets[i].Entries {
+			candidates = append(candidates, e)
+
+			if len(candidates) == t.root.K {
+				return
+			}
+		}
+	}
+
+	return
 }
 
 func (t *RoutingTable) Contains(node NodeID) bool {
@@ -40,24 +103,24 @@ func (t *RoutingTable) Contains(node NodeID) bool {
 }
 
 type bucket struct {
-	K     int
-	Low   NodeID
-	High  NodeID
-	Left  *bucket
-	Right *bucket
-	Owner NodeID
-	Nodes []NodeID
+	K       int
+	Low     NodeID
+	High    NodeID
+	Left    *bucket
+	Right   *bucket
+	Owner   NodeID
+	Entries []RoutingEntry
 }
 
-func (b *bucket) AllNodes() []NodeID {
+func (b *bucket) AllNodes() []RoutingEntry {
 	if b.Left != nil && b.Right != nil {
-		nodes := []NodeID{}
+		nodes := []RoutingEntry{}
 		nodes = append(nodes, b.Left.AllNodes()...)
 		nodes = append(nodes, b.Right.AllNodes()...)
 		return nodes
 	}
 
-	return b.Nodes
+	return b.Entries
 }
 
 func (b *bucket) Contains(node NodeID) bool {
@@ -69,8 +132,8 @@ func (b *bucket) Contains(node NodeID) bool {
 		return b.Right.Contains(node)
 	}
 
-	for _, n := range b.Nodes {
-		if node == n {
+	for _, n := range b.Entries {
+		if node == n.NodeID {
 			return true
 		}
 	}
@@ -83,19 +146,19 @@ func (b *bucket) WithinRange(node NodeID) bool {
 }
 
 // Insert returns true if the node is inserted and false otherwise.
-func (b *bucket) Insert(node NodeID) bool {
+func (b *bucket) Insert(entry RoutingEntry) bool {
 	// If bucket has been split, recurse down the tree
 	if b.Left != nil && b.Right != nil {
-		if b.Left.WithinRange(node) {
-			return b.Left.Insert(node)
+		if b.Left.WithinRange(entry.NodeID) {
+			return b.Left.Insert(entry)
 		}
 
-		return b.Right.Insert(node)
+		return b.Right.Insert(entry)
 	}
 
 	// If bucket is not full, insert is done
-	if len(b.Nodes) < b.K {
-		b.Nodes = append(b.Nodes, node)
+	if len(b.Entries) < b.K {
+		b.Entries = append(b.Entries, entry)
 		return true
 	}
 
@@ -107,19 +170,19 @@ func (b *bucket) Insert(node NodeID) bool {
 	// Otherwise, split the bucket and reinsert all nodes
 	b.split()
 
-	for _, n := range b.Nodes {
-		if b.Left.WithinRange(n) {
+	for _, n := range b.Entries {
+		if b.Left.WithinRange(n.NodeID) {
 			b.Left.Insert(n)
 		} else {
 			b.Right.Insert(n)
 		}
 	}
 
-	b.Nodes = []NodeID{}
-	if b.Left.WithinRange(node) {
-		return b.Left.Insert(node)
+	b.Entries = []RoutingEntry{}
+	if b.Left.WithinRange(entry.NodeID) {
+		return b.Left.Insert(entry)
 	}
-	return b.Right.Insert(node)
+	return b.Right.Insert(entry)
 }
 
 func (b *bucket) split() {
@@ -130,17 +193,17 @@ func (b *bucket) split() {
 	mid := b.Low + ((b.High - b.Low) / 2)
 
 	b.Left = &bucket{
-		K:     b.K,
-		Low:   b.Low,
-		High:  mid,
-		Owner: b.Owner,
-		Nodes: []NodeID{},
+		K:       b.K,
+		Low:     b.Low,
+		High:    mid,
+		Owner:   b.Owner,
+		Entries: []RoutingEntry{},
 	}
 	b.Right = &bucket{
-		K:     b.K,
-		Low:   mid + 1,
-		High:  b.High,
-		Owner: b.Owner,
-		Nodes: []NodeID{},
+		K:       b.K,
+		Low:     mid + 1,
+		High:    b.High,
+		Owner:   b.Owner,
+		Entries: []RoutingEntry{},
 	}
 }
--- kademlia/routing_test.go
+++ kademlia/routing_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestRoutingTable_Simple(t *testing.T) {
+func TestRoutingTable_Insert(t *testing.T) {
 	table := NewRoutingTable(1, 13, 15)
 
 	nodes := []struct {
@@ -22,7 +22,7 @@ func TestRoutingTable_Simple(t *testing.T) {
 	}
 
 	for _, n := range nodes {
-		inserted := table.Insert(n.ID)
+		inserted := table.Insert(RoutingEntry{n.ID, ""})
 		if inserted != n.Inserted {
 			t.Fatal("node inserted is wrong")
 		}
@@ -35,19 +35,98 @@ func TestRoutingTable_Simple(t *testing.T) {
 	}
 
 	allNodes := table.Nodes()
+
 	for _, n := range nodes {
 		if n.Inserted {
+			found := false
 			for _, nn := range allNodes {
-				if nn == n.ID {
-					return
+				if nn.NodeID == n.ID {
+					found = true
+					break
 				}
 			}
-			t.Fatal("node list is wrong")
+			if !found {
+				t.Fatalf("node list is wrong, doesn't contain %d", n.ID)
+			}
 		} else {
 			for _, nn := range allNodes {
-				if nn == n.ID {
-					t.Fatal("node list is wrong")
+				if nn.NodeID == n.ID {
+					t.Fatalf("node list is wrong, contains %d", n.ID)
 				}
+			}
+		}
+	}
+}
+
+func TestRoutingTable_DistantNodes(t *testing.T) {
+	table := NewRoutingTable(1, 13, 15)
+
+	nodes := []struct {
+		ID NodeID
+	}{
+		{1},
+		{10},
+		{6},
+		{9},
+		{12},
+		{15},
+	}
+
+	for _, n := range nodes {
+		table.Insert(RoutingEntry{n.ID, ""})
+	}
+
+	distant := table.DistantNodes(3)
+	if len(distant) != 1 {
+		t.Fatal("there should only be 1 distant node")
+	}
+	if distant[0].NodeID != NodeID(1) {
+		t.Fatal("wrong distant node")
+	}
+}
+
+func TestRoutingTable_FindNode(t *testing.T) {
+	table := NewRoutingTable(1, 13, 15)
+
+	nodes := []struct {
+		ID NodeID
+	}{
+		{1},
+		{10},
+		{6},
+		{9},
+		{12},
+		{15},
+	}
+
+	for _, n := range nodes {
+		table.Insert(RoutingEntry{n.ID, ""})
+	}
+
+	tests := []struct {
+		Target     NodeID
+		Candidates []NodeID
+	}{
+		{3, []NodeID{1}},
+		{6, []NodeID{1}},
+		{8, []NodeID{10}},
+		{11, []NodeID{10}},
+		{14, []NodeID{15}},
+	}
+
+	for _, tt := range tests {
+		candidates := table.FindNode(tt.Target)
+		for _, cc := range tt.Candidates {
+			found := false
+			for _, c := range candidates {
+				if cc == c.NodeID {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				t.Fatalf("wrong candidate, should contain %d", cc)
 			}
 		}
 	}
@@ -61,7 +140,7 @@ func TestRoutingTable_Big(t *testing.T) {
 		table := NewRoutingTable(k, owner, 1<<64-1)
 
 		for i := 0; i < 1000; i++ {
-			table.Insert(genNodeID())
+			table.Insert(RoutingEntry{genNodeID(), ""})
 		}
 	}
 }
